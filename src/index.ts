e06e968dd877f8a462ff0cdd3792b2873d78ee0d
Vadim Homchik <homchik@gmail.com>
2026-01-25 13:11:04 +0100

    Add layout builder helpers

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,17 @@ export * from "./types.js";
 export * from "./generators.js";
 export * from "./processor.js";
 export * from "./utils.js";
+
+// Re-export hastscript for convenience
+export { h } from "hastscript";
+
+// Helpers to create special nodes
+import type { SegmentNode, TextNode } from "./types.js";
+
+export function s(id: string): SegmentNode {
+  return { type: "segment", id };
+}
+
+export function t(value: string): TextNode {
+  return { type: "text", value };
+}
